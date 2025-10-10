43052e5799f4dc53ec89436c74da808a25ac091b
PradeepKumar673 <165313608+pradeepkumar673@users.noreply.github.com>
2025-10-10 10:54:52 +0530

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,13 +1,19 @@
-import express from "express"
-import { getCurrentUser, updateUserLocation } from "../controllers/user.controllers.js"
-import isAuth from "../middlewares/isAuth.js"
+import express from "express";
+import {
+  getCurrentUser,
+  updateUserLocation,
+  sendOtp,        
+  verifyOtp       
+} from "../controllers/user.controllers.js";
 
+import isAuth from "../middlewares/isAuth.js";
 
-const userRouter=express.Router()
+const userRouter = express.Router();
 
 // OTP Routes
-userRouter.post('/send-otp', sendOtp)
-userRouter.post('/verify-otp', verifyOtp)
-userRouter.get("/current",isAuth,getCurrentUser)
-userRouter.post('/update-location',isAuth,updateUserLocation)
-export default userRouter
+userRouter.post("/send-otp", sendOtp);
+userRouter.post("/verify-otp", verifyOtp);
+userRouter.get("/current", isAuth, getCurrentUser);
+userRouter.post("/update-location", isAuth, updateUserLocation);
+
+export default userRouter;
